d6761c2d12428e7dc2cb0d85c415ccd8e543a1fb
Miasoul <74416346+Miasoul@users.noreply.github.com>
2021-01-13 11:05:07 +0900

    Update test.py

--- test.py
+++ test.py
@@ -7,19 +7,21 @@ from discord.utils import get
 
 
 client = discord.Client()
-client = commands.Bot(command_prefix="~")
+
+@client.event
+async def on_message(message):
+    site = ['https','http']
+    role = discord.utils.get(message.guild.roles, name = "링크")
+    for i in site:
+        if i in message.content:
+            if message.author.guild_permissions.manage_channels:
+                return
+            elif role in message.author.roles:
+                return
+            else:
+                await message.delete()  
 
 
-@client.command()
-async def ban(ctx, member : discord.Member, reason):
-    channel = '792606160419029022'
-    embed = discord.Embed(title="처리결과", description="", color=0x62c1cc)
-    embed.add_field(name="이름",value=member.name)
-    embed.add_field(name="사유",value=reason)
-    embed.add_field(name="처벌",value="밴")
-    await member.ban(reason = reason)
-    await client.get_channel(int(channel)).send(embed=embed)
-    await ctx.message.delete()
 
 acces_token = os.environ["BOT_TOKEN"]    
 client.run(acces_token)    
